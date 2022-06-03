7f1cd37cedce7bac980c6eb3a091da0ba65c9932
MB <moreaccess@outlook.com>
2022-06-03 15:07:41 +0300

    Update Mailbox Permissions - all users except self.ps1

--- Mailbox Permissions - all users except self.ps1
+++ Mailbox Permissions - all users except self.ps1
@@ -46,7 +46,7 @@ Connect-ExchangeOnline -ShowBanner:$false
 Install-Module ImportExcel -Scope CurrentUser -ErrorAction SilentlyContinue #Will install import excel module, otherwise it will not be possible to export to excel.
 $date = (get-date -UFormat "%Y-%m-%d (%H-%M-%S)") #Gets date and time for excel file name.
 #$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition #Detects current folder frome where are you executing script, if localy execute
-$scriptPath = c:\temp
+$scriptPath = "%Desktop%\$ExcelFileName"
 $ExcelFileName = "O365-Get-MailboxPermission" #Excel name
 $ExcelFile = "$scriptPath\$ExcelFileName-$date.xlsx" #Genereates excel file location and name
 #EXPORT TO Excel
